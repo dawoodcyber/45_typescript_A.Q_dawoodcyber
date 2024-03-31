06ffb44bc8ea18341d9e2e4c09c0e08584b6e584
dawoodcyber <ds.khi.pk@gmail.com>
2024-03-31 20:23:09 +0500

    45 Assignment questions

---
+++ 1.ts
@@ -0,0 +1,5 @@
+//Node.js
+//TypeScript
+//VS Code
+//has successfully installed!
+console.log("Hello, World!");
---
+++ 22.ts
@@ -0,0 +1,14 @@
+let alpha:string[] = ["England", "Australia", "Japan", "Germany", "United States"];
+// arrray indices starts from 0 in typescript, and length should be -1 because indices starts from 0
+for(let k=1; k<=alpha.length; k++)
+{
+    console.log(alpha[k]);
+
+}
+// arrray indices starts from 0 in typescript, and length should be -1 because indices starts from 0
+console.log("\nCORRECT CODE")
+for(let k=0; k<alpha.length; k++)
+{
+    console.log(alpha[k]);
+
+}
